3f7a3ee4f6a9deb18431c60563e068fc8bee7212
Bin Gao <gaobin@icadyptes.cs.pdx.edu>
2015-02-24 11:58:42 -0800

    merge lab-home

--- lab6/example1.s
+++ lab6/example1.s
@@ -2,16 +2,6 @@
 	.text
 	.globl	f
 f:
-        movl    $0, %eax        # initialize length count in eax
-        jmp     test
-loop:   incl    %eax            # increment count
-        addq    $4, %rdi        # and move to next array element
-
-test:   movl    (%rdi), %ecx    
-        cmpl    $0, %ecx        
-        jne     loop            # repeat if we're not done ...
-
-
 	movl $0, %eax  		# initialize cnt = 0
 L:	
 	movl (%rdi), %ecx  	# load array element
